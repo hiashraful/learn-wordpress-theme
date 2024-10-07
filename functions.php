1e5df94eec5d6ac5b5d15135f6250d5457149a9b
hiashraful <hiashraful@gmail.com>
2024-10-07 20:33:21 +0600

    Environment setup, css js files loaded

---
+++ functions.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * Theme Functions
+ */
+
+// Theme Title
+add_theme_support('title-tag');
+
+// Theme CSS & jQuery File calling
+function devash_call_css_js() { 
+    //css files
+    wp_enqueue_style('devash-style', get_stylesheet_uri());
+    wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.css', array(), '5.3.3', 'all');
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_style('devash-custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+
+    //jQuery files
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('bootstrap', get_template_directory_uri() . '/js/bootstrap.js', array(), '5.3', true);
+    wp_enqueue_script('main', get_template_directory_uri() . '/js/main.js', array(), '1.0.0', true);
+
+}
+add_action('wp_enqueue_scripts', 'devash_call_css_js');
+
